44feb9eeb5de2197e503479974a0813a0d9622ee
Yi Ou <ouyi@gmx.de>
2016-09-17 19:47:18 +0000

    Finish UI and start js

--- app/app.js
+++ app/app.js
@@ -1,20 +1,16 @@
-// Creating a new Vue instance and pass in an options object.
-var demo = new Vue({
-    
-    // A DOM element to mount our view model.
-    el: '#main',
 
-        // This is the model.
-    // Define properties and give them initial values.
+var topLeft = new Vue({
+    el: '#top-l',
     data: {
-        active: 'home'
+        lightened: false
     },
-
-    // Functions we will be using.
     methods: {
-        makeActive: function(item){
-            // When a model is changed, the view will be automatically updated.
-            this.active = item;
+        lighten: function() {
+            this.lightened = true
+        },
+        darken: function() {
+            this.lightened = false
         }
     }
 });
+
